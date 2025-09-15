8623599e6368265db2ff466b326d09d36b3e8662
Semir Zahirovic <semir_guzonja@hotmail.se>
2025-09-15 10:36:24 +0200

    inte klar

---
+++ hälsa användaren/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace hälsa_användaren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello("World");
+            Console.ReadLine();
+        }
+        static void SayHello(string name);
+        {
+        
+
+    }
+}
